7ff733484fd9c6e8d765b211b17b3ac87eec4e68
tippeltappel <git@tm-r.de>
2022-10-07 00:03:43 +0200

    -

--- test.py
+++ test.py
@@ -1,24 +1,49 @@
-import py_compile
-
-
-# test.py_compile
+# test.py
 import json
+from json import JSONEncoder
+from dataclasses import dataclass
 
-data = {"user":
-    {"name":"Thorsten",
-    "age":62}
-    }
+@dataclass
+class Unit:
+    name: str
+    rank: int
 
-print(data)    
+@dataclass
+class Category:
+    name: str
+    rank: int
 
-with open ("test.json","w") as file_1:
-    json.dump(data,file_1,indent=4)
+@dataclass
+class Freezer:
+    units: list[Unit]
+    categories: list[Category]
 
-#json_str= json.dumps(data,indent=2)
-json_str= json.dumps(data,separators=(", ",": "))
-#print(json_str)
+class FreezerEncoder(JSONEncoder):
+    def default(self, freezer):
+        return ({"units":freezer.units},{"categories":freezer.categories})
 
-with open ("test.json","r") as file_2:
-    data=json.load(file_2)
+# filepathes
+units_file = "data/test.json"
 
-print(data)      
+# test data
+freezer = Freezer
+freezer.units = [{"name":"ml","rank":88},{"name":"gr","rank":88}]   
+freezer.categories = [{"name":"Gemüse","rank":11},{"name":"Fleisch","rank":22}]   
+
+def units_obj_save(file_name):
+    with open (file_name,"w") as f:
+        f.write(jsonpickle.encode(freezer))
+
+def units_obj_load(file_name):
+    with open (file_name,"r") as f:
+        return json.load(f)
+
+print(freezer.units)
+print(freezer.categories)
+JSONStr=json.dumps(freezer,indent=4,cls=FreezerEncoder)
+print(JSONStr)
+freezer=json.loads(JSONStr,cls=FreezerEncoder)
+#units_obj_save(units_file) 
+#freezer.units=units_obj_load(units_file)       
+print(freezer.units)
+print(freezer.categories)
