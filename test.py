78687db4442687077468e9902a8f7e99e726424f
tippeltappel <git@tm-r.de>
2022-10-07 09:43:44 +0200

    -

--- test.py
+++ test.py
@@ -20,30 +20,33 @@ class Freezer:
 
 class FreezerEncoder(JSONEncoder):
     def default(self, freezer):
-        return ({"units":freezer.units},{"categories":freezer.categories})
+        return ({"units":freezer.units,"categories":freezer.categories})
+
+class FreezerDecoder(JSONEncoder):
+    def default(self, js):
+        return ({"units":freezer.units},{"categories":freezer.categories})        
 
 # filepathes
-units_file = "data/test.json"
+freezer_file = "data/test.json"
 
 # test data
 freezer = Freezer
 freezer.units = [{"name":"ml","rank":88},{"name":"gr","rank":88}]   
 freezer.categories = [{"name":"Gemüse","rank":11},{"name":"Fleisch","rank":22}]   
 
-def units_obj_save(file_name):
+def freezer_obj_save(file_name):
     with open (file_name,"w") as f:
-        f.write(jsonpickle.encode(freezer))
+        json.dump(freezer,f,ensure_ascii=False,indent=4,cls=FreezerEncoder)
 
 def units_obj_load(file_name):
     with open (file_name,"r") as f:
         return json.load(f)
 
-print(freezer.units)
-print(freezer.categories)
-JSONStr=json.dumps(freezer,indent=4,cls=FreezerEncoder)
+JSONStr=json.dumps(freezer,ensure_ascii=False,indent=4,cls=FreezerEncoder)
 print(JSONStr)
-freezer=json.loads(JSONStr,cls=FreezerEncoder)
+freezer_obj_save(freezer_file)
+#freezer=json.loads(JSONStr,cls=FreezerEncoder)
 #units_obj_save(units_file) 
 #freezer.units=units_obj_load(units_file)       
-print(freezer.units)
-print(freezer.categories)
+#print(freezer.units)
+#print(freezer.categories)
