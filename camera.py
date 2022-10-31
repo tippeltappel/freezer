4cc6fdbc169016991f4c890af5153ecb6a935193
tippeltappel <git@tm-r.de>
2022-10-31 10:29:50 +0100

    -

--- camera.py
+++ camera.py
@@ -2,4 +2,13 @@ import streamlit as st
 
 
 
-st.camera_input("Aufnahme")
+img=st.camera_input("Aufnahme")
+
+if img:
+    st.image(img)
+    
+file = st.file_uploader("Choose a text file")
+if file:
+    #bytes_data = uploaded_file.read()
+    st.write("filename:", file.name)
+    #st.write(bytes_data)
